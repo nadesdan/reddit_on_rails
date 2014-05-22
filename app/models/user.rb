92fc6c473b5321a97815c2a617b95af4dd61f93d
Daniel Benjamin <nadesdan@gmail.com>
2014-05-22 19:06:38 +0200

    link model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-	attr_accessor :email, :username
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :links
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+	attr_accessible :email, :username, :password, :password_confirmation, :remember_me
 end
